d78605c38b102c1e45f1b17c41c594d561aa356c
molinaja <juan.molina.cr@gmail.com>
2021-02-23 07:03:33 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleParalleAndThreads
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string fileName = "result.txt";
+            var fs = new FileStream(fileName, FileMode.Append, FileAccess.Write);
+            Parallel.For (1, 100, new ParallelOptions { MaxDegreeOfParallelism = 10 },
+                (i) => {
+                    Write(fileName, i, fs);
+                }) ;
+
+        }
+
+        public static void Write(string path, int id, FileStream fs )
+        {
+
+            HttpClient clientHttp = new HttpClient();
+            var reponse = clientHttp.GetAsync("https://jsonplaceholder.typicode.com/todos/"+id).Result;
+            var content = reponse.Content.ReadAsStringAsync().Result + Environment.NewLine;
+            lock (fs)
+            {
+                byte[] bContent = new UTF8Encoding(true).GetBytes(content + Environment.NewLine);
+                fs.Write(bContent, 0,  bContent.Length);
+                fs.Flush();
+            }
+
+        }
+
+
+    }
+
+
+}
